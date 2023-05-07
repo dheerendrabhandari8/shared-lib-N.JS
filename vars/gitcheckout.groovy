8383d26ef76d802dc5ea216b9f6a88fa7fbaaa99
dheerendra <dheerendra>
2023-05-07 03:13:10 +0000

    added

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -2,6 +2,7 @@ def call(Map stageParams){
     checkout ([
         $class: 'GitSCM',
         branches: [[name: stageParams.branch]],
-        userRemoteConfigs: [[url: stageParams.url]]
+
+        userRemoteConfigs: [[credentialsId: 2b4d0ff7-2b8a-42bc-9e50-8e02a10bfaee, url: stageParams.url]]
     ])
 }
