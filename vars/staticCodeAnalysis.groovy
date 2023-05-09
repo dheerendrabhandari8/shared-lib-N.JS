d03a3fafb26e446809fef9dbe93cb0d1718c6d9e
dheerendra <dheerendra>
2023-05-09 12:07:28 +0000

    sonar

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,5 +1,11 @@
 def call (){
+
+
+
     withSonarQubeEnv(credentialsId: 'sonarQube') {
-    sh 'npm install sonar-scanner'
+   sh "${scannerHome}/bin/sonar-scanner"
+
+   sh 'npm install sonar-scanner'
     }
+
 }
